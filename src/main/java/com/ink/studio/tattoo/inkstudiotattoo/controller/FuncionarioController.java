a914c7e3498531483630c000a4f2446c840cb128
Gabriel Santos <bielss1609@gmail.com>
2024-08-29 08:04:07 -0300

    Ajustando para aparecer imagem

--- src/main/java/com/ink/studio/tattoo/inkstudiotattoo/controller/FuncionarioController.java
+++ src/main/java/com/ink/studio/tattoo/inkstudiotattoo/controller/FuncionarioController.java
@@ -6,7 +6,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
